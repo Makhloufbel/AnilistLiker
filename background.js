732319960b74ed1885cf5f597336ae8760626e4e
Makhloufbel <68123929+Makhloufbel@users.noreply.github.com>
2022-02-05 10:54:27 +0000

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+﻿// background.js
+chrome.action.onClicked.addListener((tab) => {
+if(tab.url.includes('anilist.co')){chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
+}
+else
+{chrome.tabs.create({url: "https://www.anilist.co"});}
+});
