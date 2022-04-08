8b259c9f6635cab0dbf5606860b535866b94179b
Makhloufbel <68123929+Makhloufbel@users.noreply.github.com>
2022-04-08 22:18:05 +0000

    Update background.js
    removed :like all post when clicking on the extention

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
-﻿// background.js
+// background.js
 chrome.action.onClicked.addListener((tab) => {
-if(tab.url.includes('anilist.co')){chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['content.js']
-  });
-}
-else
-{chrome.tabs.create({url: "https://www.anilist.co"});}
+    if (tab.url.includes('anilist.co')) {
+        /*         chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content.js'],
+        }); */
+    } else {
+        chrome.tabs.create({ url: 'https://www.anilist.co' });
+    }
 });
